08131c1959ce8d2147e89aeb3e9ab74cb65ed601
ritikjaiswal88 <ritikjaiswal8839@gmail.com>
2025-06-21 12:35:26 +0530

    final fix

--- src/app.js
+++ src/app.js
@@ -1,5 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+// --- UNCOMMENT ALL YOUR ORIGINAL IMPORTS ---
 import Header from "./Components/header";
 import Body from "./Components/body";
 import Help from "./Components/Help";
@@ -10,28 +11,36 @@ import { createBrowserRouter,RouterProvider,Outlet } from "react-router-dom";
 import { Provider } from "react-redux";
 import appStore from "./utils/appStore";
 import Cart from "./Components/cart";
+// --- END UNCOMMENT ---
 
 
-const AppLayout = ()=> {
+const AppLayout = () => {
     return (
-    <Provider store={appStore}>
-    <div className="app">
-        <Header/>
-        <Outlet/>
-    </div>
-    </Provider>
+        <Provider store={appStore}>
+            <div className="app">
+                {/* --- UNCOMMENT Header --- */}
+                <Header/>
+                {/* --- UNCOMMENT Outlet --- */}
+                <Outlet/>
+                {/* REMOVE THE TEST TEXT */}
+                {/* <h1>Router Test Page!</h1> */}
+                {/* <p>If you see this, router setup works for the default path.</p> */}
+            </div>
+        </Provider>
     );
 };
 
 const appRouter = createBrowserRouter([
     {
-        path:"/",
-        element:<AppLayout/>,
-        children:[
+        path: "/",
+        element: <AppLayout/>,
+        children: [
             {
-                path:"/",
+                path: "/",
+                // --- BRING BACK Body COMPONENT ---
                 element:<Body/>
             },
+            // --- BRING BACK ALL YOUR ORIGINAL ROUTES ---
             {
                 path:"/corporate",
                 element:<Corporate/>
@@ -49,12 +58,12 @@ const appRouter = createBrowserRouter([
                 element:<Cart/>
             },
         ],
+        // --- BRING BACK Error COMPONENT ---
         errorElement:<Error/>
     },
-    
-])
+], {
+    basename: "/foodOdring/"
+});
 
- 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-
 root.render(<RouterProvider router={appRouter}/>);
